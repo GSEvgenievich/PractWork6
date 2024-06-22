0f27c2fd62f7961507b50bc7e3f97ad8946b5e98
geras <Stepan.geras2006@yandex.ru>
2024-06-22 12:28:40 +0300

    Добавьте файлы проекта.

---
+++ Task1/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Game
+    {
+        public int Sale { get; set; } = 10;
+        public int IdGame { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public string PriceWithSale { get => $"{Price:0.00} rub./{Price*Sale:0.00}"; }
+    }
+}
